24a1e37d1b87de0e26a0dcc35137a60941606b94
Caio Monte Santo Barbosa <110570422+Cai0Sant0@users.noreply.github.com>
2024-05-13 19:27:31 -0300

    Fazendo modificações na caixa

--- Cxmsg.js
+++ Cxmsg.js
@@ -16,8 +16,53 @@ class Cxmsg{
     }
     
    mostrar=()=>{
-    console.log(this.texto);
-   }
+    this.divmsg = document.createElement("div");
+
+    const estilo_divmsg = 
+    "display: flex;"+
+    "justify-content: center;"+
+    "align-items: center;"+
+    "position: absolute;"+
+    "top: 0px;"+
+    "left: 0px;"+
+    "width: 100%;"+
+    "height: 100vh;"+
+    "background-color: rgba(0,0,0,0.7);";
+
+    this.divmsg.setAttribute("id","divmsg");
+    this.divmsg.setAttribute("style", estilo_divmsg);
+    this.destino.prepend(this.divmsg);
+
+    let areaCaixaMsg = document.createElement("div");
+
+    const estilo_areaCaixaMsg =
+    "display: flex;"+
+    "justify-content: flex-start;"+
+    "align-items: flex-start;"+
+    "flex-direction: column;"+
+    "width: 300px";
+
+    areaCaixaMsg.setAttribute("style", estilo_areaCaixaMsg);
+
+    this.divmsg.appendChild(areaCaixaMsg);
+
+    let divTitulo = document.createElement("div");
+
+    const estiloTitulo = 
+    "display: flex;"+
+    "justify-content: flex-start"+
+    "align-items: center;"+
+    "width: 100%;"+
+    "background-color:"+this.cor+";"+
+    "color:#fff;"+
+    "padding:5px;"+
+    "border-radius: 5px 5px 0px 0px;";
+
+    divTitulo.setAttribute("style", estiloTitulo);
+    divTitulo.innerHTML = this.titulo;
+    areaCaixaMsg.appendChild(divTitulo);
+
+   };
 
    ocultar=()=>{
 
